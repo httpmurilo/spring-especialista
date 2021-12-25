2b6a3a6108fe174c63f5068b2f38b14856270afb
Murilo Eduardo Saldanha R de Melo <murilo.melo@softplan.com.br>
2021-12-25 19:11:54 -0300

    jdbc exemplos

--- src/main/java/io/murilo/vendas/VendasApplication.java
+++ src/main/java/io/murilo/vendas/VendasApplication.java
@@ -8,19 +8,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class VendasApplication {
+
 
     @Bean
     public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
         return args -> {
-            Cliente cliente = new Cliente();
-            cliente.setNome("Murilo");
+            Cliente cliente = new Cliente("Murilo");
             clienteRepository.salvar(cliente);
+
+            Cliente cliente2 = new Cliente("Murilo Eduardo");
+            clienteRepository.salvar(cliente2);
+
+            List<Cliente> clienteList = new ArrayList<>();
+            clienteList = clienteRepository.obterTodos();
+            clienteList.forEach(cliente1 -> {
+                System.out.println(cliente1.getId());
+                System.out.println(cliente1.getNome());
+            });
         };
     }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
+
+
 }
--- src/main/java/io/murilo/vendas/domain/entity/Cliente.java
+++ src/main/java/io/murilo/vendas/domain/entity/Cliente.java
@@ -5,6 +5,17 @@ public class Cliente {
     private Integer id;
     private String nome;
 
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+    public Cliente() {
+    }
+
     public Integer getId() {
         return id;
     }
