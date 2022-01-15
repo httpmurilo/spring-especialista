668d6006625d292b279e52e77bf3d4d73c501ed7
httpmurilo <muriloeduardo1997@outlook.com>
2022-01-14 21:17:23 -0300

    continuação jwt

--- src/main/java/io/murilo/vendas/services/JwtService.java
+++ src/main/java/io/murilo/vendas/services/JwtService.java
@@ -1,10 +1,21 @@
 package io.murilo.vendas.services;
 
+import io.murilo.vendas.domain.entity.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class JwtService {
 
+    @Value("${security.jwt.expiracao}")
     private String expiracao;
+    @Value("${security.jwt.chave-assinatura}")
     private String chaveAssinatura;
+
+    public String gerarToken(Usuario usuario) {
+        long expiracaoString = Long.parseLong(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expiracaoString);
+    }
 }
